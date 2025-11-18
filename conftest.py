7dc7c812d573e2dfd7bb07708a90222f3adb25d7
Mikhail <karpov.vlg@gmail.com>
2025-11-18 17:26:05 +0300

    try CICD 0.3

--- conftest.py
+++ conftest.py
@@ -1,9 +1,10 @@
+import os
+import time
+
+import asyncpg
+import docker
 import pytest
 import pytest_asyncio
-import docker
-import time
-import asyncpg
-
 from clients.pg_client import AsyncPostgresClient
 
 POSTGRES_IMAGE = "postgres:15"
@@ -13,43 +14,50 @@ POSTGRES_DB = "test_db"
 CONTAINER_NAME = "test_pg_container"
 PORT = 5433
 
+
 @pytest.fixture(scope="session")
 def docker_postgres():
-    client = docker.from_env()
-
-    container = client.containers.run(
-        POSTGRES_IMAGE,
-        name=CONTAINER_NAME,
-        environment={
-            "POSTGRES_USER": POSTGRES_USER,
-            "POSTGRES_PASSWORD": POSTGRES_PASSWORD,
-            "POSTGRES_DB": POSTGRES_DB
-        },
-        ports={"5432/tcp": PORT},
-        detach=True,
-        remove=True 
-    )
-
-    time.sleep(3)
-
-    for _ in range(10):
-        try:
-            conn = asyncpg.connect(
-                user=POSTGRES_USER,
-                password=POSTGRES_PASSWORD,
-                database=POSTGRES_DB,
-                host="localhost",
-                port=PORT
-            )
-            break
-        except Exception:
-            time.sleep(1)
+    if "DB_DSN" in os.environ:
+        yield os.environ["DB_DSN"]
     else:
-        container.stop()
-        pytest.fail("PostgreSQL в Docker не поднялся")
+        client = docker.from_env()
+        try:
+            client.containers.get(CONTAINER_NAME).stop()
+        except docker.errors.NotFound:
+            pass
 
-    yield f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@localhost:{PORT}/{POSTGRES_DB}"
-    container.stop()
+        container = client.containers.run(
+            POSTGRES_IMAGE,
+            name=CONTAINER_NAME,
+            environment={
+                "POSTGRES_USER": POSTGRES_USER,
+                "POSTGRES_PASSWORD": POSTGRES_PASSWORD,
+                "POSTGRES_DB": POSTGRES_DB,
+            },
+            ports={"5432/tcp": PORT},
+            detach=True,
+        )
+
+        time.sleep(3)
+
+        for _ in range(10):
+            try:
+                conn = asyncpg.connect(
+                    user=POSTGRES_USER,
+                    password=POSTGRES_PASSWORD,
+                    database=POSTGRES_DB,
+                    host="localhost",
+                    port=PORT,
+                )
+                break
+            except Exception:
+                time.sleep(1)
+        else:
+            container.stop()
+            pytest.fail("PostgreSQL в Docker не поднялся")
+
+        yield f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@localhost:{PORT}/{POSTGRES_DB}"
+        container.stop()
 
 
 @pytest_asyncio.fixture(scope="module")
@@ -64,4 +72,5 @@ async def db(docker_postgres):
 @pytest_asyncio.fixture()
 async def actioner(db):
     from clients.async_user_actioner import AsyncUserActioner
+
     return AsyncUserActioner(db)
